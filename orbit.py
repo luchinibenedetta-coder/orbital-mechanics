f96f30d0a5f35dfb504cd69ee02293214919d4a0
luchinibenedetta-coder <luchinibenedetta@gmail.com>
2026-08-27 01:30:16 +0200

    Update orbit.py
    Calculate gravitational acceleration

--- orbit.py
+++ orbit.py
@@ -11,6 +11,14 @@ y = 0.0               # initial y position [m]
 vx = 0.0              # initial x velocity [m/s]
 vy = 7546.0           # initial y velocity [m/s]
 
+# Distance from Earth's center
+r = (x**2 + y**2)**0.5
+
+# Gravitational acceleration
+ax = -G * M_earth * x / r**3
+ay = -G * M_earth * y / r**3
+
+print("Gravitational acceleration:", ax, ay, "m/s^2")
 print("Orbital mechanics simulation")
 print("Initial position:", x, y, "m")
 print("Initial velocity:", vx, vy, "m/s"))
