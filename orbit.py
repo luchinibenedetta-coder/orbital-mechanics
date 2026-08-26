2c7a91a4f4b2e3f2482bf9bf53a25774460a832d
luchinibenedetta-coder <luchinibenedetta@gmail.com>
2026-08-27 01:27:31 +0200

    Update orbit.py
    define Earth and satellite initial conditions

--- orbit.py
+++ orbit.py
@@ -1,1 +1,16 @@
+print(# Orbital Mechanics Simulation
+
+# Physical constants
+G = 6.67430e-11       # gravitational constant [m^3 kg^-1 s^-2]
+M_earth = 5.972e24    # mass of Earth [kg]
+
+# Initial conditions
+x = 7.0e6             # initial x position [m]
+y = 0.0               # initial y position [m]
+
+vx = 0.0              # initial x velocity [m/s]
+vy = 7546.0           # initial y velocity [m/s]
+
 print("Orbital mechanics simulation")
+print("Initial position:", x, y, "m")
+print("Initial velocity:", vx, vy, "m/s"))
