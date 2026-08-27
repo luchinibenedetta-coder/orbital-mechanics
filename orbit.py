8a12853c945bafd2be73c8fa20c6b7a8756e262a
luchinibenedetta-coder <luchinibenedetta@gmail.com>
2026-08-27 18:45:53 +0200

    Update orbit.py
    fix Orbital simulation code

--- orbit.py
+++ orbit.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-print(# Orbital Mechanics Simulation
+# Orbital Mechanics Simulation
 
 # Physical constants
 G = 6.67430e-11       # gravitational constant [m^3 kg^-1 s^-2]
@@ -11,13 +11,6 @@ x = 7.0e6             # initial x position [m]
 y = 0.0               # initial y position [m]
 vx = 0.0              # initial x velocity [m/s]
 vy = 7546.0           # initial y velocity [m/s]
-
-# Distance from Earth's center
-r = (x**2 + y**2)**0.5
-
-# Gravitational acceleration
-ax = -G * M_earth * x / r**3
-ay = -G * M_earth * y / r**3
 
 # Simulation settings
 dt = 1.0          # time step [s]
@@ -45,8 +38,8 @@ for t in range(total_time):
     x = x + vx * dt
     y = y + vy * dt
 
-#Store corrente position
-x_positions.appendi(x)
+#Store current position
+x_positions.append(x)
 y_position.apendi(y)
 
 print("Final position:", x, y, "m")
